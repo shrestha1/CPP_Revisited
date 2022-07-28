89a61eac0cab0255391ce1619989ede151e8e240
shrestha1 <newardipesh@gmail.com>
2022-07-28 13:37:13 +0545

    added create Board function

--- snake/main.cpp
+++ snake/main.cpp
@@ -3,13 +3,18 @@
 
 #include "board.h"
 
+std::unique_ptr<Board> createBoard(std::pair<int, int> size){
+    return std::make_unique<Board>(size);
+}
+
 int main(){
     std::pair <int, int> size(255, 255);
-    std::unique_ptr<Board> board = std::make_unique<Board> (size);
+    std::unique_ptr<Board> board = createBoard(size);
 
     int x = board->getSize().first;
     int y = board->getSize().second;
 
     std::cout<<"X: "<<x<<", Y: "<<y<<std::endl;
+    std::cout<<board.get()<<std::endl; //memory of board
     return 0;
 }
